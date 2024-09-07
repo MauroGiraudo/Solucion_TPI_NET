58d3f0cd99431d2fb7620c564caaf6b31c21b4b0
Joaquipe13 <jfernandezd@frro.utn.edu.ar>
2024-09-07 12:05:01 -0300

    BonicficacionService

---
+++ Domain/Services/BonificacionService.cs
@@ -0,0 +1,56 @@
+using Domain.Model.Compras;
+using ProgramaPrincipal;
+
+namespace Domain.Services
+{
+    public class BonificacionService
+    {
+        public void Add(Bonificacion bonificacion)
+        {
+            using var context = new TiendaRopaContext();
+
+            context.Bonificaciones.Add(bonificacion);
+            context.SaveChanges();
+        }
+
+        public void Update(Bonificacion bonificacion)
+        {
+            using var context = new TiendaRopaContext();
+
+            Bonificacion? bonificacionToUpdate = context.Bonificaciones.Find(bonificacion.IdBonificacion);
+
+            if (bonificacionToUpdate != null)
+            {
+                bonificacionToUpdate.CantidadParaDescuento = bonificacion.CantidadParaDescuento;
+                bonificacionToUpdate.ProporcionDescuento = bonificacion.ProporcionDescuento;
+                bonificacionToUpdate.FecVigenciaBonificacion = bonificacion.FecVigenciaBonificacion;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(int idBonificacion)
+        {
+            using var context = new TiendaRopaContext();
+
+            Bonificacion? bonificacionToDelete = context.Bonificaciones.Find(bonificacion.IdBonificacion);
+
+            if (bonificacionToDelete != null)
+            {
+                context.Bonificaciones.Remove(bonificacionToDelete);
+                context.SaveChanges();
+            }
+        }
+
+        public Bonificacion? GetOne(int idBonificacion)
+        {
+            using var context = new TiendaRopaContext();
+            return context.Bonificaciones.Find(IdBonificacion);
+        }
+
+        public IEnumerable<Bonificacion> FindAll()
+        {
+            using var context = new TiendaRopaContext();
+            return context.Bonificaciones.ToList();
+        }
+    }
+}
